c018a3094726d14e701210c7ce3a0f20be18d996
t <tysonpederson95@gmail.com>
2022-08-24 14:14:07 -0500

    Make buttons look like they're being pushed

--- calculator.js
+++ calculator.js
@@ -17,3 +17,29 @@ function divide(a, b) {
 function operate(operator, a, b) {
     return operator(a, b);
 }
+
+// Make buttons look like they're being pushed
+const buttons = document.querySelectorAll("button");
+for(button of buttons) {
+    button.addEventListener("mousedown", buttonDown);
+    button.addEventListener("mouseup", buttonUp);
+    button.addEventListener("mouseleave", buttonUp);
+}
+function buttonDown(button) {
+    if (button.target.classList.contains("operator")) {
+        button.target.classList.add("buttonDownOperator");
+    } else if (button.target.classList.contains("number")) {
+        button.target.classList.add("buttonDownNumber");
+    } else if (button.target.classList.contains("clear")) {
+        button.target.classList.add("buttonDownClear");
+    }
+}
+function buttonUp(button) {
+    if (button.target.classList.contains("operator")) {
+        button.target.classList.remove("buttonDownOperator");
+    } else if (button.target.classList.contains("number")) {
+        button.target.classList.remove("buttonDownNumber");
+    } else if (button.target.classList.contains("clear")) {
+        button.target.classList.remove("buttonDownClear");
+    }
+}
